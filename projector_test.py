3bd13c0df9bb8ac613c126f8a3e6081527ed6867
bekama <kat.maurer@fau.de>
2023-06-24 12:13:20 +0200

    tmp: testing projector features

--- projector_test.py
+++ projector_test.py
@@ -1,11 +1,13 @@
 ### based on the quickstart example ###
 import blenderproc as bproc
 import numpy as np
+from PIL import Image
+
 
 bproc.init()
 
 # Create a simple object:
-obj = bproc.object.create_primitive("CUBE")
+obj = bproc.object.create_primitive("MONKEY")
 obj.set_location([0,-0,0])
 #print(obj.get_location())
 #print(obj.get_rotation_euler())
@@ -19,16 +21,20 @@ obj.set_location([0,-0,0])
 # Set the camera to be in front of the object
 cam_pose = bproc.math.build_transformation_mat([0, -5, 0], [np.pi / 2, 0, 0])
 bproc.camera.add_camera_pose(cam_pose)
+bproc.camera.set_stereo_parameters(interocular_distance=0.05, convergence_mode="PARALLEL")
 
 # Create projector next to camera
-#proj = bproc.loader.load_obj("resources/projector.obj")
-proj = bproc.loader.load_obj("resources/strip.obj")
-proj[0].set_location([1, -2, 0])
-proj[0].set_rotation_euler([0,0,0])
-#proj[0].set_scale([2,2,4])
-proj[0].set_rotation_mat([[np.cos(45), -np.sin(45), 0],[np.sin(45),np.cos(45), 0],[0, 0, 1]])
-bproc.lighting.light_surface(proj, 10)
+proj = bproc.types.Light()
+proj.set_type('SPOT')
+proj.set_energy(3000)
+fov = bproc.camera.get_fov()
+pattern_img = np.asarray(Image.open("resources/stips.png"))
 
+#roj.set_location([1, -2, 0])
+#proj.set_rotation_euler([0,0,0])
+#roj.set_rotation_mat([[np.cos(45), -np.sin(45), 0],[np.sin(45),np.cos(45), 0],[0, 0, 1]])
+
+proj.setup_as_projector(pattern_img)
 # Render the scene
 data = bproc.renderer.render()
 
