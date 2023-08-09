3cb8e319058306825a1362a46dcbda497cceb182
bekama <kat.maurer@fau.de>
2023-08-09 17:32:38 +0200

    ref: remove outdated code

--- loop.py
+++ loop.py
@@ -9,14 +9,4 @@ def loop():
 		command = f"blenderproc run randomroom.py -o test --full"
 		process = subprocess.run(command.split(), stdout=subprocess.PIPE)
 
-	# # patterns
-	# for i in tqdm(range(10)):
-	# 	command = f"blenderproc run randomroom.py -o test/PATTERN -proj -pat points"
-	# 	process = subprocess.run(command.split(), stdout=subprocess.PIPE)
-
-	# # patterns infrared
-	# for i in tqdm(range(10)):
-	# 	command = f"blenderproc run randomroom.py -o test/INFRARED -proj -pat points --infrared"
-	# 	process = subprocess.run(command.split(), stdout=subprocess.PIPE)
-
 	print("Done generating images")
