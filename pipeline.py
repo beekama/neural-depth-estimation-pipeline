070e7bc394329ef157ab40bbf1c5577ef1954545
Kathrin Maurer <kat.maurer@fau.de>
2023-08-22 15:00:29 +0200

    feat: set number of loops within pipeline

--- pipeline.py
+++ pipeline.py
@@ -13,7 +13,8 @@ from extract_images import extract_images
 from depthestimation import depthestimation
 from loop import loop
 
-RAW_FOLDER = "pipeline_1"
+RAW_FOLDER = "pipeline_0"
+IMAGES = 250
 POSES = 5
 EPOCHES = 10
 
@@ -25,11 +26,12 @@ if __name__ == "__main__":
     parser.add_argument('--loop', action=argparse.BooleanOptionalAction, help='enable/disable image-generation', default=False)
     parser.add_argument('--output_dir', '-o', help='name of outputfolder', default=RAW_FOLDER)    
     parser.add_argument('--num_poses', '-poses', help='Number of poses within one frame', default=POSES)
+    parser.add_argument('--num_images', '-images', help='Number of created frames', default=IMAGES)
 
     args = parser.parse_args()
     
     if (args.loop):
-        loop(args.output_dir, args.num_poses)
+        loop(args.output_dir, args.num_poses, args.num_images)
     
     extract_images(args.output_dir + '/NORMALOS/', 'neuronalDepthEst/' + args.output_dir + '/NORMALOS')
     extract_images(args.output_dir + '/PATTERN/', 'neuronalDepthEst/' + args.output_dir + '/PATTERN')
