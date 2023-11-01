26478810d69ab2e8b515b8df6a7c6593ca349912
Kathrin Maurer <kat.maurer@fau.de>
2023-11-01 23:56:54 +0100

    feat: save errormaps folder

--- scripts/calculate_error.py
+++ scripts/calculate_error.py
@@ -2,6 +2,12 @@ import os
 import argparse
 import cv2 as cv
 import numpy as np
+import torch
+import torchvision.transforms as transforms
+from torchmetrics.image import StructuralSimilarityIndexMeasure as  SSIM
+from torchmetrics.regression import MeanSquaredError
+
+
 
 FOLDER = ""
 PATTERN = r"(\d{3})_depth"
@@ -9,6 +15,7 @@ PATTERN = r"(\d{3})_depth"
 def calculate_mse(folder, pattern, save_maps):
 
     number_total = 0
+    ssim_total = 0
     mse_total = 0
     mse_min = ""
     mse_min_val = -1
@@ -29,34 +36,44 @@ def calculate_mse(folder, pattern, save_maps):
     for number, files in sorted_files.items():
         if len(files) != 2:
             raise ValueError(f"wrong number of files with num: {number}")
-        print(f"filenumber: {number}")
-        for file in files:
-            print(f"- {file}")
-        abs_diff = cv.absdiff(cv.imread(os.path.join(folder, files[0]), cv.IMREAD_GRAYSCALE), cv.imread(os.path.join(folder, files[1]), cv.IMREAD_GRAYSCALE))
-        squared_error = abs_diff.astype(np.float32) ** 2
-        mse_pp = np.mean(squared_error)
+        
+        # convert to tensor
+        transform = transforms.Compose([transforms.ToTensor()])
+        img1 = cv.imread(os.path.join(folder, files[0]), cv.IMREAD_GRAYSCALE)
+        img2 = cv.imread(os.path.join(folder, files[1]), cv.IMREAD_GRAYSCALE)
+
+        img1_tensor = transform(img1).unsqueeze(0)
+        img2_tensor = transform(img2).unsqueeze(0)
+    
+        mse = MeanSquaredError()(img1_tensor, img2_tensor)
+        ssim = SSIM()(img1_tensor, img2_tensor)
 
         if save_maps:
+            abs_diff = cv.absdiff(img1, img2)
+            squared_error = abs_diff.astype(np.float32) ** 2
+            # normalize and apply colormap
             norm_error = cv.normalize(squared_error, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
             errormap = cv.applyColorMap(norm_error, cv.COLORMAP_JET)
-            cv.imwrite(f"{number:03}_errormap.png", errormap)
+            cv.imwrite(os.path.join(folder,f"{int(number):03d}_errormap.png"), errormap)
 
-        print(f"meansquared: {mse_pp}")
-        if mse_pp < mse_min_val or mse_min_val == -1:
-            mse_min = file
-            mse_min_val = mse_pp
-        if mse_pp > mse_max_val or mse_max_val == -1:
-            mse_max = file
-            mse_max_val = mse_pp
+        print(f"me:ansquared: {mse}")
+        if mse < mse_min_val or mse_min_val == -1:
+            mse_min = files[0]
+            mse_min_val = mse
+        if mse > mse_max_val or mse_max_val == -1:
+            mse_max = files[1]
+            mse_max_val = mse
             
-        mse_total += mse_pp
+        mse_total += mse
+        ssim_total += ssim
         if int(number) > number_total:
             number_total = int(number)
     mse_total /= number_total
+    ssim_total /= number_total
 
     # save in file
     with open(os.path.join(folder, result_file), 'a') as file:
-        msg = f"MSE: {mse_total:.3f}\nMSE_min: {mse_min}: {mse_min_val:.3f}\nMSE_max: {mse_max}: {mse_max_val:.3f}\n"
+        msg = f"MSE: {mse_total:.3f}\nMSE_min: {mse_min}: {mse_min_val:.3f}\nMSE_max: {mse_max}: {mse_max_val:.3f}\nSSIM: {ssim_total:.3f}\n"
         file.write(msg)
 
 
